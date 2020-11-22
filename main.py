01ef597e5e9f094eabc464210a80fbf0798b9109
mkeari <mkeari17@gmail.com>
2020-11-22 19:55:08 +0600

    refined

--- main.py
+++ main.py
@@ -4,35 +4,32 @@ import numpy as np
 import math
 from random import random
 
-# Initial position of a robot
-p_initial = [3, 3]
-theta_initial = 0
+# Initial position and parameters of a robot
+p_initial = [13, 13]
+theta_initial = 1
 
 R_initial = 0.2
 L_initial = 1.5
 dT_initial = 0.05
 
+robot_speed = 3
+prox_sensor_radius = 0.5
+
 # Light source position
 p_light = [17, 16]
 
 # Centre coordinates of the obstacles
+obstacle_radius = 1.5
 p_obstacles = [[6, 7],
                [5, 16],
                [14, 8],
                [18, 3]]
 
-# simulation parameters
-Kp_rho = 9
-Kp_alpha = 15
-Kp_beta = -3
-dt = 0.01
 
-show_animation = True
-
-
+#Class of mobile robot
 class mobile_robot():
-    def __init__(self, x_initial, y_initial, theta_initial,
-                 light_position, R=0.2, L=1.3, dT=0.1):
+    def __init__(self, x_initial, y_initial, theta_initial, 
+                base_speed = 1.5, R=0.2, L=1.3, dT=0.1, ps_radius = 0.5):
         self.x = x_initial
         self.y = y_initial
         self.theta = theta_initial
@@ -41,9 +38,11 @@ class mobile_robot():
         self.L = L
         self.dT = dT
 
-        self.v_rwheel = 2
-        self.v_lwheel = 2
+        self.base_speed = base_speed
+        self.v_rwheel = 0
+        self.v_lwheel = 0
 
+        self.prox_sensor_radius = ps_radius
         self.fl_sensor = 0
         self.sl_sensor = 0
         self.fr_sensor = 0
@@ -52,6 +51,17 @@ class mobile_robot():
         self.l_light = 0
         self.r_light = 0
 
+    
+    def move(self):
+        self.forward_kinematics()
+        self.calc_velocities()
+
+
+    def stop(self):
+        self.v_lwheel = 0
+        self.v_rwheel = 0
+
+    
     def forward_kinematics(self):
         R = (self.L/2)*(self.v_lwheel + self.v_rwheel) / \
             (self.v_rwheel - self.v_lwheel + 0.00001)
@@ -67,117 +77,88 @@ class mobile_robot():
                            math.cos(w*dt) * (self.y - ICC[1]) + ICC[1])
         self.theta = self.theta + w*dt
 
-    def calc_velocities(self, light_position):
-        #self.check_obstacles()
-        ls_left = self.x
+    
+    def calc_velocities(self):
+        self.check_obstacles()
+        self.check_light()
+        
+        self.v_lwheel = self.base_speed + self.l_light + \
+                        5*self.fl_sensor - 5*self.fr_sensor + 8*self.sl_sensor
+        
+        self.v_rwheel = self.base_speed + self.r_light + \
+                        5*self.fr_sensor - 5*self.fl_sensor + 8*self.sr_sensor
 
-    def check_light(self, light_position):
+
+    def check_light(self):
+        #Defining light sensor positions
         T = transformation_matrix(self.x, self.y, self.theta)
         p_ls_left = np.array([0.4 * self.L, 0.15*self.L, 1]).T
         p_ls_right = np.array([0.4 * self.L, -0.15*self.L, 1]).T
         ls_left = np.matmul(T, p_ls_left)
         ls_right = np.matmul(T, p_ls_right)
 
-        dist_left = math.hypot(light_position[0] - ls_left[0], 
-                               light_position[1] - ls_left[1])
-        dist_right = math.hypot(light_position[0] - ls_right[0], 
-                               light_position[1] - ls_right[1])
-        return dist_left, dist_right
+        #Calculating distances from each sensor to light source
+        dist_left = math.hypot(p_light[0] - ls_left[0], 
+                               p_light[1] - ls_left[1])
+        dist_right = math.hypot(p_light[0] - ls_right[0], 
+                               p_light[1] - ls_right[1])
 
+        #Configuring light sensor values 
+        dist_average = (dist_left + dist_right)/2
+        self.l_light = 100*(dist_left-dist_right)/dist_average
+        self.r_light = 100*(dist_right-dist_left)/dist_average
+
+        #Printing for debug
+        #print("Left: " + str(self.l_light) + "   Right: " + str(self.r_light))
+
+    
     def check_obstacles(self):
+        #Define positions of proximity sensors
         T = transformation_matrix(self.x, self.y, self.theta)
-        p_fl = np.array([0.6 * robot.L, 0.30*robot.L, 1]).T
-        p_fr = np.array([0.6 * robot.L, -0.30*robot.L, 1]).T
-        p_sl = np.array([0.2 * robot.L, 0.6*robot.L, 1]).T
-        p_sr = np.array([0.2 * robot.L, -0.6*robot.L, 1]).T
+        p_fl = np.array([0.6 * self.L, 0.30*self.L, 1]).T
+        p_fr = np.array([0.6 * self.L, -0.30*self.L, 1]).T
+        p_sl = np.array([0.2 * self.L, 0.6*self.L, 1]).T
+        p_sr = np.array([0.2 * self.L, -0.6*self.L, 1]).T
 
         ps_fl = np.matmul(T, p_fl)
         ps_fr = np.matmul(T, p_fr)
         ps_sl = np.matmul(T, p_sl)
         ps_sr = np.matmul(T, p_sr)
 
+        #Find nearest obstacle
+        min_dist = 40
+        nearest_obstacle = [20, 20]
+        for obstacle in p_obstacles:
+            dist = math.hypot(self.x - obstacle[0], self.y - obstacle[1])
+            if dist <= min_dist:
+                nearest_obstacle = obstacle
+                min_dist = dist
+
+        #print("Nearest obstacle at " + str(nearest_obstacle))
+
+        #Calculate the closeness of nearest obstacle
+        self.fl_sensor = self.check_prox_sensor(ps_fl, nearest_obstacle, 
+                                            self.prox_sensor_radius, obstacle_radius)
+        self.fr_sensor = self.check_prox_sensor(ps_fr, nearest_obstacle, 
+                                            self.prox_sensor_radius, obstacle_radius)
+        self.sl_sensor = self.check_prox_sensor(ps_sl, nearest_obstacle, 
+                                            self.prox_sensor_radius, obstacle_radius)
+        self.sr_sensor = self.check_prox_sensor(ps_sr, nearest_obstacle, 
+                                            self.prox_sensor_radius, obstacle_radius)                                            
         
+        #Printing sensor values
+        #print("FL: " + str(self.fl_sensor) + "   FR: " + str(self.fr_sensor) +
+        #    "\nSL: " + str(self.sl_sensor) + "   SR: " + str(self.sr_sensor))
 
-def move_robot(p_initial, theta_initial, light_position):
-    x_position = x_initial
-    y_position = y_initial
-    theta = theta_initial
-
-    v_left, vright = robot.calc_velocities()
-
-    x_diff = light_position - x_position
-    y_diff = light_position - y_position
-
-    x_traj, y_traj = [], []
-
-    dist = np.hypot(x_diff, y_diff)
-
-    #while dist > 0.05:
-
-
-def move_to_pose(x_start, y_start, theta_start, x_goal, y_goal, theta_goal):
-    """
-    rho is the distance between the robot and the goal position
-    alpha is the angle to the goal relative to the heading of the robot
-    beta is the angle between the robot's position and the goal position plus the goal angle
-
-    Kp_rho*rho and Kp_alpha*alpha drive the robot along a line towards the goal
-    Kp_beta*beta rotates the line so that it is parallel to the goal angle
-    """
-    x = x_start
-    y = y_start
-    theta = theta_start
-
-    x_diff = x_goal - x
-    y_diff = y_goal - y
-
-    x_traj, y_traj = [], []
-
-    rho = np.hypot(x_diff, y_diff)
-    while rho > 0.001:
-        x_traj.append(x)
-        y_traj.append(y)
-
-        x_diff = x_goal - x
-        y_diff = y_goal - y
-
-        # Restrict alpha and beta (angle differences) to the range
-        # [-pi, pi] to prevent unstable behavior e.g. difference going
-        # from 0 rad to 2*pi rad with slight turn
-
-        rho = np.hypot(x_diff, y_diff)
-        alpha = (np.arctan2(y_diff, x_diff)
-                 - theta + np.pi) % (2 * np.pi) - np.pi
-        beta = (theta_goal - theta - alpha + np.pi) % (2 * np.pi) - np.pi
-
-        v = Kp_rho * rho
-        w = Kp_alpha * alpha + Kp_beta * beta
-
-        if alpha > np.pi / 2 or alpha < -np.pi / 2:
-            v = -v
-
-        theta = theta + w * dt
-        x = x + v * np.cos(theta) * dt
-        y = y + v * np.sin(theta) * dt
-
-        if show_animation:  # pragma: no cover
-            plt.cla()
-
-            fig = plt.gcf()
-            ax = fig.gca()
-
-            plt.arrow(x_start, y_start, np.cos(theta_start),
-                      np.sin(theta_start), color='r', width=0.1)
-
-            # Drawing obstacle circles
-            for obstacle in p_obstacles:
-                circle = plt.Circle((obstacle[0],
-                                     obstacle[1]), 1.5, color='dodgerblue')
-                ax.add_artist(circle)
-
-            plt.arrow(x_goal, y_goal, np.cos(theta_goal),
-                      np.sin(theta_goal), color='g', width=0.1)
-            plot_vehicle(x, y, theta, x_traj, y_traj)
+    
+    def check_prox_sensor(self, sensor_position, obstacle_position, 
+                          sensor_radius, obstacle_radius):
+        dist = math.hypot(obstacle_position[0] - sensor_position[0],
+                          obstacle_position[1] - sensor_position[1])
+        if dist <= (sensor_radius + obstacle_radius):
+            return 1/dist
+        else:
+            return 0
 
 
 def transformation_matrix(x, y, theta):
@@ -188,15 +169,7 @@ def transformation_matrix(x, y, theta):
     ])
 
 
-def place_obstacle(x, y, size, color="-b"):  # pragma: no cover
-    deg = list(range(0, 360, 5))
-    deg.append(0)
-    xl = [x + size * math.cos(np.deg2rad(d)) for d in deg]
-    yl = [y + size * math.sin(np.deg2rad(d)) for d in deg]
-    plt.plot(xl, yl, color)
-
-
-def calc_light_field(light_position, reso):
+def calc_light_field(reso):
     minx = 0
     miny = 0
     maxx = 200
@@ -204,8 +177,8 @@ def calc_light_field(light_position, reso):
     xw = int(round((maxx - minx) / reso))
     yw = int(round((maxy - miny) / reso))
 
-    light_position[0] = light_position[0] * 10
-    light_position[1] = light_position[1] * 10
+    light_position[0] = p_light[0] * 10
+    light_position[1] = p_light[1] * 10
 
     # calc each potential
     pmap = [[0.0 for i in range(yw)] for i in range(xw)]
@@ -226,8 +199,8 @@ def draw_heatmap(data):
     plt.pcolor(data, cmap=plt.cm.Blues)
 
 
-def plot_vehicle(robot, fig, ax):  # pragma: no cover
-    # Corners of triangular vehicle when pointing to the right (0 radians)
+def plot_vehicle(robot, fig, ax):  
+    #Draw rectangle base of a robot
     p1_i = np.array([0.6 * robot.L, 0.30*robot.L, 1]).T
     p2_i = np.array([-0.6 * robot.L, 0.30*robot.L, 1]).T
     p3_i = np.array([-0.6 * robot.L, -0.30*robot.L, 1]).T
@@ -280,10 +253,20 @@ def plot_vehicle(robot, fig, ax):  # pragma: no cover
 
 
     #Draw proximity sensors
-    sensor_fl = plt.Circle((p1[0], p1[1]), robot.L/10, color='r')
-    sensor_sl = plt.Circle((p4[0], p4[1]), robot.L/10, color='r')
-    sensor_fr = plt.Circle((p1_w[0], p1_w[1]), robot.L/10, color='r')
-    sensor_sr = plt.Circle((p4_w[0], p4_w[1]), robot.L/10, color='r')
+    ps_color_detected = 'r'
+    ps_plot_color = ['g', 'g', 'g', 'g']
+    sensors = [robot.fl_sensor, robot.fr_sensor, robot.sl_sensor, robot.sr_sensor]
+    
+    i = 0
+    for sensor in sensors:
+        if(sensor > 0):
+            ps_plot_color[i] = ps_color_detected
+        i = i + 1
+    
+    sensor_fl = plt.Circle((p1[0], p1[1]), robot.L/10, color=ps_plot_color[0])
+    sensor_sl = plt.Circle((p4[0], p4[1]), robot.L/10, color=ps_plot_color[1])
+    sensor_fr = plt.Circle((p1_w[0], p1_w[1]), robot.L/10, color=ps_plot_color[2])
+    sensor_sr = plt.Circle((p4_w[0], p4_w[1]), robot.L/10, color=ps_plot_color[3])
     ax.add_artist(sensor_fl)
     ax.add_artist(sensor_sl)
     ax.add_artist(sensor_fr)
@@ -297,9 +280,10 @@ def plot_vehicle(robot, fig, ax):  # pragma: no cover
     plt.xlim(0, 20)
     plt.ylim(0, 20)
 
-    plt.pause(dt)
+    plt.pause(robot.dT)
 
-def plot_everything(x_start, y_start, theta_start, robot, light_position):
+
+def plot_everything(x_start, y_start, theta_start, robot, obs_radius = 1.5):
     plt.cla()
 
     #Create the figure
@@ -311,33 +295,37 @@ def plot_everything(x_start, y_start, theta_start, robot, light_position):
     np.sin(theta_start), color='r', width=0.1)
 
     #Drawing the light source
-    light = plt.Circle(light_position, 0.3, color='y')
+    light = plt.Circle(p_light, 0.3, color='y')
     ax.add_artist(light)
     
-    #pmap = calc_light_field(light_position, 10)
+    #pmap = calc_light_field(10)
     #draw_heatmap(pmap)
 
     # Drawing obstacle circles
     for obstacle in p_obstacles:
         circle = plt.Circle((obstacle[0],
-                            obstacle[1]), 1.5, color='dodgerblue')
+                            obstacle[1]), obs_radius, color='dodgerblue')
         ax.add_artist(circle)
 
     #Drawing the robot
     plot_vehicle(robot, fig, ax)
 
+
+
 def main():
 
-    robot1 = mobile_robot(p_initial[0], p_initial[1], theta_initial, p_light, 
-                          R_initial, L_initial, dT_initial)
-    for i in range(1000):
-        robot1.forward_kinematics()
-        print(robot1.check_light(p_light))
-        # print("X: " + str(robot1.position_x) + 
-        #     "\nY: " + str(robot1.position_y) + 
-        #     "\nTh: " + str(robot1.theta))
+    robot1 = mobile_robot(p_initial[0], p_initial[1], theta_initial, robot_speed, 
+                          R_initial, L_initial, dT_initial, prox_sensor_radius)
+    
+    dist = math.hypot(robot1.x - p_light[0], robot1.y - p_light[1])
+    while dist > 0.8:
+        dist = math.hypot(robot1.x - p_light[0], robot1.y - p_light[1])
+        print(str(dist))
+
+        robot1.move()
         plot_everything(p_initial[0], p_initial[1], theta_initial, 
-                        robot1, p_light)        
+                        robot1, obstacle_radius)    
+    robot1.stop()    
 
 
 
